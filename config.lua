e69a635dea061741bdf5097233257007fc374ca7
Genah <102344886+calvinwekoila@users.noreply.github.com>
2024-07-16 17:58:39 +0700

    Max Top Speed Update
    sorting vehicle class for VercityRP

--- config.lua
+++ config.lua
@@ -1,7 +1,107 @@
 Vehicles = {
-    ['GLOBAL'] = 200, -- IF THIS IS SET TO ANYTHING BUT NIL IT WILL OVERRIDE ALL OTHER LIMITS
-    [GetHashKey('benson')] = 148,
-    [GetHashKey('biff')] = 123,
-    [GetHashKey('hauler')] = 163,
-    [GetHashKey('mule')] = 130
+    ['GLOBAL'] = 170, 
+
+-- X Class = 270 (RP Car 'not for sale')
+-- S Class = 240
+-- A Class = 220
+-- B Class = 190
+-- C Class = 170
+-- M Class = 220
+
+
+-- S Class Donation
+    [GetHashKey('vercyskyline')] = 240,
+    [GetHashKey('ikx3_xxs24b')] = 240,
+    [GetHashKey('manscarreragt')] = 240,
+    [GetHashKey('vercya80')] = 240,
+    [GetHashKey('rmodlfa')] = 240,
+    [GetHashKey('manssupersnake')] = 240,
+
+-- S Class PDM
+    [GetHashKey('coquette4')] = 240,
+    [GetHashKey('tenf')] = 240,
+    [GetHashKey('jester4')] = 240,
+    [GetHashKey('deviant')] = 240,
+    [GetHashKey('sultanrs')] = 240,
+    [GetHashKey('tenf2')] = 240,
+    [GetHashKey('comet6')] = 240, --BELOM DITUNING
+
+-- S Class Police
+
+    [GetHashKey('fed18muscle')] = 240,
+    [GetHashKey('fed20muscle')] = 240,
+    [GetHashKey('fed18sedan')] = 240,
+
+-- A Class Donation
+    [GetHashKey('ikx3_civicr23b')] = 220,
+    [GetHashKey('golf75r')] = 220,
+    [GetHashKey('fk8hr')] = 220,
+    [GetHashKey('benze55')] = 220,
+
+-- A Class PDM
+    [GetHashKey('dominator3')] = 220,
+    [GetHashKey('gauntlet')] = 220,
+    [GetHashKey('gauntlet2')] = 220,
+    [GetHashKey('comet2')] = 220,
+    [GetHashKey('elegy2')] = 220,
+    [GetHashKey('furoregt')] = 220,
+    [GetHashKey('jester3')] = 220,
+    [GetHashKey('massacro')] = 220,
+    [GetHashKey('penumbra2')] = 220,
+    [GetHashKey('zr350')] = 220,
+    [GetHashKey('vacca')] = 220,
+    [GetHashKey('tailgater2')] = 220,
+    [GetHashKey('kuruma')] = 220,
+    [GetHashKey('z190')] = 220,
+    [GetHashKey('ninef')] = 220,
+    [GetHashKey('feltzer2')] = 220,
+    [GetHashKey('warrener')] = 220,
+    [GetHashKey('comet3')] = 220, --BELOM DITUNING
+    [GetHashKey('italigto')] = 220, --BELOM DITUNING
+    [GetHashKey('elegy')] = 220, --BELOM DITUNING
+
+-- A Class Police
+    [GetHashKey('fed14sedan')] = 220,
+    [GetHashKey('fed13sedan')] = 220,
+    [GetHashKey('fed13utility')] = 220,
+    [GetHashKey('fed13utilityk9')] = 220,
+
+-- B Class PDM
+    [GetHashKey('sultan')] = 190,
+    [GetHashKey('sultan2')] = 190,
+    [GetHashKey('sultan3')] = 190,
+    [GetHashKey('stratum')] = 190,
+    [GetHashKey('superd')] = 190,
+    [GetHashKey('buffalo2')] = 190,
+    [GetHashKey('buffalo')] = 190,
+    [GetHashKey('jackal')] = 190,
+    [GetHashKey('cog55')] = 190,
+    [GetHashKey('cognoscenti')] = 190,
+    [GetHashKey('zion')] = 190,
+    [GetHashKey('dominator')] = 190,
+    [GetHashKey('sugoi')] = 190,
+    [GetHashKey('blista')] = 190,
+    [GetHashKey('neon')] = 190, --BELOM DITUNING
+
+-- C Class PDM
+    [GetHashKey('asterope')] = 170,
+    [GetHashKey('premier')] = 170,
+    [GetHashKey('intruder')] = 170,
+    [GetHashKey('sentinel')] = 170,
+    [GetHashKey('primo2')] = 170,
+    [GetHashKey('surge')] = 170,
+    [GetHashKey('emperor')] = 170,
+    [GetHashKey('fugitive')] = 170,
+    [GetHashKey('glendale')] = 170,
+    [GetHashKey('ingot')] = 170,
+    [GetHashKey('schwarzer')] = 170,
+    [GetHashKey('felon')] = 170,
+    [GetHashKey('sugoi')] = 170,
+    [GetHashKey('blista')] = 170,
+    [GetHashKey('taxi')] = 170, --BELOM DITUNING
+    [GetHashKey('mule')] = 170, --BELOM DITUNING
+    [GetHashKey('mule2')] = 170, --BELOM DITUNING
+
+
+
 }
